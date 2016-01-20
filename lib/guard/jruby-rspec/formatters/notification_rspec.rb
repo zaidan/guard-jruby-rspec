73c40b59b1c6e557ae3abf2cc6d90bb281571db4
Firas Zaidan <firas@zaidan.de>
2016-01-20 21:23:04 +0100

    Fix renamed formatter class

--- lib/guard/jruby-rspec/formatters/notification_rspec.rb
+++ lib/guard/jruby-rspec/formatters/notification_rspec.rb
@@ -1,20 +1,21 @@
-require "guard/rspec/formatter"
+require "guard/rspec_formatter"
 
-superclass = if Guard::RSpec::Formatter.instance_of? Module # guard-rspec-1.x
+superclass = if Guard::RSpecFormatter.instance_of? Module # guard-rspec-1.x
                Object
-             elsif Guard::RSpec::Formatter.instance_of? Class # guard-rspec-2.x
-               Guard::RSpec::Formatter
+             elsif Guard::RSpecFormatter.instance_of? Class # guard-rspec-2.x
+               Guard::RSpecFormatter
              else
-               fail 'Guard::RSpec::Formatter is neither class nor module'
+               fail 'Guard::RSpecFormatter is neither class nor module'
              end
 
-class Guard::JRubyRSpec::Formatter::NotificationRSpec < superclass
-  include Guard::RSpec::Formatter if Guard::RSpec::Formatter.instance_of? Module
+class Guard::JRubyRSpec::Formatter
+  class Guard::JRubyRSpec::Formatter::NotificationRSpec < superclass
+    include Guard::RSpecFormatter if Guard::RSpecFormatter.instance_of? Module
 
-  def dump_summary(duration, total, failures, pending)
-    message = guard_message(total, failures, pending, duration)
-    image   = guard_image(failures, pending)
-    notify(message, image)
+    def dump_summary(duration, total, failures, pending)
+      message = guard_message(total, failures, pending, duration)
+      image   = guard_image(failures, pending)
+      notify(message, image)
+    end
   end
-
 end
